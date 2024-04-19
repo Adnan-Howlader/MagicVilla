d633edf5cddeb6c34050ba5f6fd7e017e666d1bc
adnanhowlader <adnanhowlader14@gmail.com>
2024-04-19 09:38:14 +0600

    defining models

--- MagicVilla_VillaApi/Models/Villa.cs
+++ MagicVilla_VillaApi/Models/Villa.cs
@@ -5,34 +5,25 @@ namespace MagicVilla_VillaApi.Models;
 
 public class Villa
 {
-    
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-   
-    public string Name{ get; set; }
-    
+
+    public string Name { get; set; }
+
     public string Details { get; set; }
-    
-    [Required]
-    public double rate { get; set; }
-    
-    [Required]
-    public int sqft { get; set; }
-    
-    [Required]
-    public bool occupancy { get; set; }
-    
+
+    [Required] public double rate { get; set; }
+
+    [Required] public int sqft { get; set; }
+
+    [Required] public bool occupancy { get; set; }
+
     public string imageurl { get; set; }
-    
+
     public string amenity { get; set; }
-    
-    
-    
+
     public DateTime CreatedDate { get; set; }
-    
+
     public DateTime UpdateDate { get; set; }
-    
-    
-    
 }
